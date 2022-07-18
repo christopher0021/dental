5509a72f709914537823377633927790c7be0c4a
christopher0021 <107992281+christopher0021@users.noreply.github.com>
2022-07-18 16:33:27 +0800

    Update edit.php

--- Dental Inventory System/edit.php
+++ Dental Inventory System/edit.php
@@ -1,39 +1,60 @@
+<?php  
+
+	include('config/db_conn.php');
+
+	if(isset($_POST['submit'])){
+
+		$name = mysqli_real_escape_string($conn, $_POST['name']);
+		$qty = mysqli_real_escape_string($conn, $_POST['qty']);	
+		$id = mysqli_real_escape_string($conn, $_POST['id']);	
+		
+		$sql = "UPDATE inventory 
+						SET Product_Name='$name', Product_Stock='$qty' 
+						WHERE id=$id ";
+
+		$result = mysqli_query($conn, $sql);
+
+			if($result){
+				header('location: home.php');
+
+				}else{
+					echo '';			
+		}
+	}
+
+		if (isset($_GET['id'])) {
+
+		$id = mysqli_real_escape_string($conn, $_GET['id']);
+
+		$sql = "SELECT * FROM inventory WHERE id = $id";
+
+		$result = mysqli_query($conn, $sql);
+		$edit = mysqli_fetch_assoc($result);
+
+		mysqli_free_result($result);
+		mysqli_close($conn);
+
+	}
+ ?> 
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <link rel="stylesheet" href="config/style.css">
 
 	<?php include('header.php'); ?>
-<style>
-.box
-{
-  display: flex;
-  margin: auto;
-  width: 250px;
-  height: 25px;
-}
-</style>
-  <div class="box center">
-    <form action="home.php">
-      <input type="text" placeholder="Search.." name="search">
-      <button type="submit" class="btn brand">Submit</button>
-    </form>
-  </div>
-</div>
 
-
-	<section class="container">
-		<h4 class="center">Add Supplies</h4>
-		<form class="white" action="add.php" method="POST">
+	<section class="container">		
+		<h4 class="center">Edit Supplies</h4>
+		<form class="white" action="edit.php" method="POST">
+			 <input type="text" name="id" value="<?php echo ($_GET['id']); ?>" hidden >
 			<label><h5>Product Name:</label>
 				<input type="text" name="name" value="">
 			<label><h5>Product Qty :</label>
 				<input type="number" name="qty" value="">
-			<div class="center">
+			<div class="center" method="POST">
 				<input type="submit" name="submit" value="submit" class="btn brand">
 			</input>
 			</div>
 		</form>
 	</section>
-
 </html>
